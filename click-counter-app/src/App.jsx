04d373ce62b55be818f8c063622fcc970bf4579a
Akin Holo <holoakinseloyin@gmail.com>
2025-04-19 02:25:25 +0100

    Update App.jsx

--- click-counter-app/src/App.jsx
+++ click-counter-app/src/App.jsx
@@ -5,21 +5,15 @@ import './App.css'
 
 function App() {
   const AppName = "Click Counter";
-  let threshold = "";
   const [count, setCount] = useState(0)
 
 
   const increaseCount = () => {
     setCount((count) => count + 1);
-    
-    if( count === 0 || count > 20 ) {
-      threshold.textContent = "You've reached the limit"
-    }
   }
 
   const decreaseCount = () => {
     setCount((count) => count - 1)
-
   }
 
   return (
@@ -30,8 +24,6 @@ function App() {
         <IncreaseButton count={count} increaseCount={increaseCount} />
         <DecreaseButton count={count} decreaseCount={decreaseCount} />
       </div>
-      <p>{threshold}</p>
-    
     </>
   )
 }
